ff98e33a43093bc62e9795e3486240b727bd6828
Alexander Pilz <alex.pilz95@googlemail.com>
2024-04-02 14:45:25 +0200

    Haling of more complex parameters

--- OGCProcess2Galaxy.py
+++ OGCProcess2Galaxy.py
@@ -2,7 +2,9 @@ import json
 import xml.etree.ElementTree
 import urllib.request 
 import warnings
+import sys
 
+#OGC Process Description types to Galaxy Parameter types
 typeMapping = {
   "array": "?",
   "boolean": "boolean",
@@ -12,114 +14,160 @@ typeMapping = {
   "string": "text"
 }
 
+#conformance classes 
 confClasses = [
 "http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/core",
 "http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/ogc-process-description",
 "http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/json"
 ]
 
-def OGCAPIProcesses2Galaxy(baseURL: str):
+def OGCAPIProcesses2Galaxy(configFile: str) -> None:
+    """
+    Function to convert processes form a given list of 
+    OGC API Processes instances to Galaxy tools. 
 
-    #check conformance
-    with urllib.request.urlopen(baseURL + "conformance") as conformanceURL:
-        conformanceData = json.load(conformanceURL)
-        
-        for confClass in confClasses:
-            if confClass not in confClasses:
-                msg = "Specified API available via:" + baseURL + " does not conform to " + confClass + "." + "This may lead to issues when converting its processes to Galaxy tools."
-                warnings.warn(msg, Warning)
+    Parameters:
+    configFile (str): Path to config file which specifies the instances as well as 
+    included and excluded services. Example: 
+    [
+        {
+            "server_url": "https://someOGCAPIProcessesInstace/api/",
+            "included_services": ["*"],
+            "excluded_services": ["process1", "process45"]
+        }
+    ]
+
+    Returns:
+    None: Function has no return value. Converted processes are stores as .xml-files.
+    """
     
-    #get processes
-    with urllib.request.urlopen(baseURL + "processes") as processesURL:
-        processesData = json.load(processesURL)
+    #load config
+    with open(configFile) as configFile:
+        configJSON = json.load(configFile)
+
+    for api in configJSON: 
+
+        #check conformance
+        with urllib.request.urlopen(api["server_url"] + "conformance") as conformanceURL:
+            conformanceData = json.load(conformanceURL)
+            
+            for confClass in confClasses:
+                if confClass not in confClasses:
+                    msg = "Specified API available via:" + baseURL + " does not conform to " + confClass + "." + "This may lead to issues when converting its processes to Galaxy tools."
+                    warnings.warn(msg, Warning)
         
-        for process in processesData["processes"][0:20]:
-            if len(rocessesData["processes"]) == 0:
-                msg = "Specified API available via:" + baseURL + " does not provide any processes."
-                warnings.warn(msg, Warning)
+        #get processes
+        with urllib.request.urlopen(api["server_url"] + "processes") as processesURL:
+            processesData = json.load(processesURL)
+            
+            for process in processesData["processes"]:
+                if len(processesData["processes"]) == 0:
+                    msg = "Specified API available via:" + baseURL + " does not provide any processes."
+                    warnings.warn(msg, Warning)
 
 
-            with urllib.request.urlopen(baseURL + "processes/" + process["id"]) as processURL:
+                with urllib.request.urlopen(api["server_url"] + "processes/" + process["id"]) as processURL:
+                    
+                    process = json.load(processURL)
+
+                    if(process["id"] in api["excluded_services"]):
+                        continue
+
+                    if(process["id"] in api["included_services"] or ("*" in api["included_services"] and len(api["included_services"]) == 1)):
+
+                        toolID = process["id"]
+                        toolName = process["title"]
+                        toolVersion = process["version"]
                 
-                process = json.load(processURL)
+                        isComplex = False
 
-                toolID = process["id"]
-                toolName = process["title"]
-                toolVersion = process["version"]
-        
-                #add tool
-                tool = xml.etree.ElementTree.Element("tool") 
-                tool.set('id', toolID)
-                tool.set('name', toolName)
-                tool.set('version', toolVersion)
-        
-                #add description
-                description = xml.etree.ElementTree.Element("description") 
-                description.text = process["description"]
-                tool.append(description) 
-        
-                #add inputs
-                inputs = xml.etree.ElementTree.Element("inputs") 
+                        #add tool
+                        tool = xml.etree.ElementTree.Element("tool") 
+                        tool.set('id', toolID)
+                        tool.set('name', toolName)
+                        tool.set('version', toolVersion)
                 
-                #add params
-                for param in process["inputs"]:
-                    paramID = param
+                        #add description
+                        description = xml.etree.ElementTree.Element("description") 
+                        description.text = str(process["description"])
+                        tool.append(description) 
+                
+                        #add inputs
+                        inputs = xml.etree.ElementTree.Element("inputs") 
+                        
+                        #add params
+                        for param in process["inputs"]:
+                            paramID = param
 
-                    #add param title
-                    if "title" in process["inputs"][param].keys():
-                        paramTitle = process["inputs"][param]["title"]
-                    else:
-                        msg = "Parameter " + paramID + " of process " + toolID + " has not title attribute."
-                        warnings.warn(msg, Warning)
-                        paramTitle = "?"
+                            #add param title
+                            if "title" in process["inputs"][param].keys():
+                                paramTitle = process["inputs"][param]["title"]
+                            else:
+                                msg = "Parameter " + paramID + " of process " + toolID + " has not title attribute."
+                                warnings.warn(msg, Warning)
+                                paramTitle = "?"
 
-                    if "description" in process["inputs"][param].keys():
-                        paramDescription = process["inputs"][param]["description"]
-                    else:
-                        msg = "Parameter " + paramID + " of process " + toolID + " has not description attribute."
-                        warnings.warn(msg, Warning)
-                        paramDescription = "?"
+                            if "description" in process["inputs"][param].keys():
+                                paramDescription = process["inputs"][param]["description"]
+                            else:
+                                msg = "Parameter " + paramID + " of process " + toolID + " has not description attribute."
+                                warnings.warn(msg, Warning)
+                                paramDescription = "No description provided!"
 
+                            #add param type
+                            if 'type' in process["inputs"][param]["schema"].keys(): #simple schema
+                                if process["inputs"][param]["schema"]["type"] in typeMapping.keys():
+                                    paramType = typeMapping[process["inputs"][param]["schema"]["type"]]
+                            elif 'oneOf' in process["inputs"][param]["schema"].keys(): #simple schema
+                                isComplex = True
+                                paramFormats = ""
+                                for format in process["inputs"][param]["schema"]["oneOf"]:
+                                    if format["type"] == "string":
+                                        paramType = typeMapping["string"]
+                                        paramFormats = paramFormats + ", " + format["contentMediaType"]
+                                    if format["type"] == "object":
+                                        paramType = typeMapping["object"]
+                            else:
+                                msg = "Parameter " + paramID + " of process " + toolID + " has no simple shema."
+                                warnings.warn(msg, Warning)
 
-                    #add param type
-                    if 'type' in process["inputs"][param]["schema"].keys(): #simple schema
-                        if process["inputs"][param]["schema"]["type"] in typeMapping.keys():
-                            paramType = typeMapping[process["inputs"][param]["schema"]["type"]]
-                    else:
-                        msg = "Parameter " + paramID + " of process " + toolID + " has no simple shema."
-                        warnings.warn(msg, Warning)
-                        paramType = "?"
+                            #compile param
+                            param = xml.etree.ElementTree.Element("param") 
+                            param.set('name', paramID)
+                            param.set('label', paramTitle)
+                            param.set('help', paramDescription)
+                            param.set('type', paramType)
+                            if isComplex:
+                                param.set('format', paramFormats[2:])
 
-                    #compile param
-                    param = xml.etree.ElementTree.Element("param") 
-                    param.set('name', paramID)
-                    param.set('label', paramTitle)
-                    param.set('help', paramDescription)
-                    param.set('type', paramType)
-                    inputs.append(param)
+                            inputs.append(param)
 
-                #append inputs 
-                tool.append(inputs)
+                        #append inputs 
+                        tool.append(inputs)
 
-                #add outputs
-                outputs = xml.etree.ElementTree.Element("outputs") 
+                        #add outputs
+                        outputs = xml.etree.ElementTree.Element("outputs") 
 
-                for output in process["outputs"]:
-                    outputName = output
-                    outputLabel = process["outputs"][output]["title"]
+                        for output in process["outputs"]:
+                            outputName = output
+                            outputLabel = process["outputs"][output]["title"]
 
-                    #compile output
-                    output = xml.etree.ElementTree.Element("data") 
-                    output.set('name', outputName)
-                    output.set('label', outputLabel)
-                    outputs.append(output)
+                            #compile output
+                            output = xml.etree.ElementTree.Element("data") 
+                            output.set('name', outputName)
+                            output.set('label', outputLabel)
+                            outputs.append(output)
 
-                #append outputs
-                tool.append(outputs)
+                        #append outputs
+                        tool.append(outputs)
 
-                #export tool 
-                tree = xml.etree.ElementTree.ElementTree(tool) 
-                with open ("Output/" + toolID + ".xml", "wb") as toolFile: 
-                    tree.write(toolFile) 
+                        #export tool 
+                        tree = xml.etree.ElementTree.ElementTree(tool) 
+                        with open ("Output/" + toolID + ".xml", "wb") as toolFile: 
+                            tree.write(toolFile) 
+                        print("--> " + toolID + " exported")
 
-OGCAPIProcesses2Galaxy("https://ospd.geolabs.fr:8300/ogc-api/")
+if __name__ == "__main__":
+    configFile = " ".join( sys.argv[1:] )
+    OGCAPIProcesses2Galaxy(configFile)
+    
--- README.md
+++
@@ -1,2 +0,0 @@
-# OGCProcess2GalaxyTool
-Converter implemented with python which can transform OGC Process Descriptions to Galaxy Tools in XML 
